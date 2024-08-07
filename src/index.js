ce44c37d49a5549616c6dd93c435278fde0f78c0
Sardor <139249785+sardornext@users.noreply.github.com>
2024-08-07 10:01:23 +0500

    feat: brackets alg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketMap = new Map(bracketsConfig);
+
+  for (let char of str) {
+    if (bracketMap.has(char)) {
+      if (stack.length > 0 && stack[stack.length - 1] === char && bracketMap.get(char) === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (stack.length === 0 || bracketMap.get(stack.pop()) !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
